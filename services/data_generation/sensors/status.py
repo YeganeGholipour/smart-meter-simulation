5881ce9561176f2d52f25a76ab4c467fe357cbf8
YeganeGholipour <yeganegholiour@gmail.com>
2025-11-18 20:32:16 +0330

    fix status options and flags for easier spark process

--- services/data_generation/sensors/status.py
+++ services/data_generation/sensors/status.py
@@ -7,22 +7,28 @@ def calculate_status(
     consumption: float, voltage: float, hour: int, base_profile: List = sensor_config.BASE_PROFILE
 ):
     base = base_profile[hour]
-    status = 0  # default normal
+
+    rare_failure = 0 
+    spike_anomaly = 0
+    low_consumption = 0
+    voltage_anomaly = 0 
 
     # Rare failure / dropout
     if random.random() < 0.0005:
-        status = 2
+        rare_failure = 1 << 0
 
     # Spike anomaly
-    elif abs(consumption - base) > 3 * 0.1 * base:
-        status = 1
+    if abs(consumption - base) > 3 * 0.1 * base:
+        spike_anomaly = 1 << 1
 
     # low consumption
-    elif consumption < 0.05 * base:
-        status = 3
+    if consumption < 0.05 * base:
+        low_consumption = 1 << 2
 
     # volatage anomaly
-    elif voltage < sensor_config.VOLTAGE_RANGE[0] or voltage > sensor_config.VOLTAGE_RANGE[1]:
-        status = 4
+    if voltage < sensor_config.VOLTAGE_RANGE[0] or voltage > sensor_config.VOLTAGE_RANGE[1]:
+        voltage_anomaly = 1 << 3
+
+    status = rare_failure | spike_anomaly | low_consumption | voltage_anomaly
 
     return status
